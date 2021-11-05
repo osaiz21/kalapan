83d8a680f97853493d9c506ff52209c941c15ed4
osaiz <osaiz@proyectoholdingvml.com>
2021-11-05 08:47:35 -0500

    Vinculacion de input Codigo de Barras

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("")
+  const [valor, setValor] = useState("")
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    // fetch(`http://localhost:3000/productos?codigo_barras=${name}`)
+    // .then(response => response.json())
+    // .then(data => setValor(data))
+    // console.error(valor)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Kalapan</code> and save to reload.
+          Edit <code>Kalapan</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>Codigo de barras
+            <input 
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
       </header>
     </div>
   );
