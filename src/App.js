0f38713a7e7baa2ad8fc1b2c640d8fae716bbf76
osaiz <osaiz@proyectoholdingvml.com>
2021-11-09 08:52:14 -0500

    version Inicial

--- src/App.js
+++ src/App.js
@@ -6,29 +6,41 @@ function App() {
   const [name, setName] = useState("")
   const [valor, setValor] = useState("")
   const handleSubmit = (event) => {
-    // event.preventDefault();
-    // fetch(`http://localhost:3000/productos?codigo_barras=${name}`)
-    // .then(response => response.json())
-    // .then(data => setValor(data))
-    // console.error(valor)
+    event.preventDefault();
+    if(!!name) {
+      fetch(`https://kalapan.herokuapp.com/productos?codigo=${name}`)
+      .then(response => response.json())
+      .then(data => {
+        setValor(data)
+        setName("")
+      })
+    }
+  }
+
+  const resultValues = () => {
+    let label = <h1></h1>
+    for (let value in  valor) {
+      label = <><h1>{valor[value].nombre}</h1><h1>$ {valor[value].valor_venta}</h1></>
+    }
+    return (label)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Kalapan</code>
-        </p>
+        <h1>Kalapan</h1>
         <form onSubmit={handleSubmit}>
-          <label>Codigo de barras
+          <label>Cod de barras 
             <input 
               type="text"
               onChange={(e) => setName(e.target.value)}
+              name='codigo'
+              value={name}
             />
           </label>
-          <input type="submit" />
+          <input type="submit" value='Consultar'/>
         </form>
+        <div>{ resultValues()} </div>
       </header>
     </div>
   );
