f9fb7188c63992268e7cbbc3ef2a421b6f696f3e
osaiz <osaiz@proyectoholdingvml.com>
2021-11-12 18:27:46 -0500

    5ta pru

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, {useState} from 'react'
-import { HashRouter as Router, Route , Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route , Routes} from 'react-router-dom'
 
 
 const  App = () => {
