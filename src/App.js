05a5c4a6ea01691f42352aa0212312201cea9af1
Oswald Andres <saizandres21@gmail.com>
2021-11-13 17:07:32 -0500

    Actualizacion de Datos

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ const  App = () => {
           body: JSON.stringify(dataGeneral)
       })
       const output = await result.json()
+      console.log(output)
       // document.getElementById('form_productos').reset()
     }catch(error){
       alert(error.message || error.stack)
